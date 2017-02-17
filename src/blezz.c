bf587e872211bee6c0da2963c98f09be74b0cc42
Dave Davenport <qball@gmpclient.org>
2017-02-17 19:46:41 +0100

    Use autotools for installing.

--- src/blezz.c
+++ src/blezz.c
@@ -1,16 +1,34 @@
+/**
+ * rofi-blezz
+ *
+ * MIT/X11 License
+ * Copyright (c) 2017 Qball Cow <qball@gmpclient.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 #include <stdlib.h>
 #include <stdio.h>
-
 #include <unistd.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <strings.h>
 #include <string.h>
-#include <ctype.h>
 #include <errno.h>
 #include <gmodule.h>
-
 
 #include <rofi/mode.h>
 #include <rofi/helper.h>
@@ -104,7 +122,7 @@ static void node_child_add ( Node *p, Node *c )
     p->num_children++;
 }
 
-static char ** get_blezz (  Mode *sw )
+static void get_blezz (  Mode *sw )
 {
     BLEZZModePrivateData *rmpd = (BLEZZModePrivateData *) mode_get_private_data ( sw );
 
@@ -169,10 +187,8 @@ static char ** get_blezz (  Mode *sw )
                         node->name = g_strdup(start);
                         start = end+1;
                         node->command = g_strdup(start);
-
                         node_child_add ( cur_dir, node );
                     }
-
                 }
             }
         }
